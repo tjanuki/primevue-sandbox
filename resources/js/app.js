69033a6556838332b3854a62db949fa2ab4346e8
tjanuki <t.januki@gmail.com>
2024-07-11 16:17:07 -0400

    Add Jetstream team

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
 import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import ToastService from 'primevue/toastservice';
+import Toast from "primevue/toast";
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+
+            })
+            .use(ToastService)
+            .component('Toast', Toast)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
