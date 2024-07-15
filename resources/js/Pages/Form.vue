72db2e9963b1039812f1c82e6064270a26250c65
tjanuki <t.januki@gmail.com>
2024-07-15 16:58:43 -0400

    Add Custom Checkbox style

--- resources/js/Pages/Form.vue
+++ resources/js/Pages/Form.vue
@@ -5,9 +5,11 @@ import InputGroup from 'primevue/inputgroup';
 import InputGroupAddon from 'primevue/inputgroupaddon';
 import {useForm} from "@inertiajs/vue3";
 import InputText from 'primevue/inputtext';
+import Checkbox from "primevue/checkbox";
 
 const form = useForm({
-    date: ''
+    date: '',
+    pizza: []
 })
 
 const submit = () => {
@@ -30,7 +32,51 @@ const submit = () => {
                     <InputText placeholder="Website"/>
                 </InputGroup>
             </div>
+
+            <div class="my-6">
+                <div class="card flex flex-wrap justify-center gap-4">
+                    <div class="flex items-center">
+                        <Checkbox v-model="form.pizza" inputId="ingredient1" name="pizza" value="Cheese"
+                        />
+                        <label for="ingredient1" class="ml-2"> Cheese </label>
+                    </div>
+                    <div class="flex items-center">
+                        <Checkbox v-model="form.pizza" inputId="ingredient2" name="pizza" value="Mushroom"
+                        />
+                        <label for="ingredient2" class="ml-2"> Mushroom </label>
+                    </div>
+                    <div class="flex items-center">
+                        <Checkbox v-model="form.pizza" inputId="ingredient3" name="pizza" value="Pepper"
+                        />
+                        <label for="ingredient3" class="ml-2"> Pepper </label>
+                    </div>
+                    <div class="flex items-center">
+                        <Checkbox v-model="form.pizza" inputId="ingredient4" name="pizza" value="Onion"
+                        />
+                        <label for="ingredient4" class="ml-2"> Onion </label>
+                    </div>
+                </div>
+            </div>
         </form>
 
     </DashboardLayout>
 </template>
+
+<style lang="scss">
+.p-checkbox {
+    .p-checkbox-box {
+        background: #999;
+        border: 2px solid #999;
+        border-radius: 4px;
+
+        &.p-highlight {
+            background: #007ad9;
+            border-color: #005bb5;
+        }
+
+        .p-checkbox-icon {
+            color: #ffffff;
+        }
+    }
+}
+</style>
