a2873cfa3828836632283d380aea464b33f13d57
tjanuki <t.januki@gmail.com>
2024-07-22 16:32:08 -0400

    Add DarkModeToggle

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,16 +15,19 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 const Noir = definePreset(Aura, {
     semantic: {
-        colorScheme: {
-            dark: {
-                primary: {
-                    color: '{indigo.400}',
-                    contrastColor: '{indigo.900}',
-                    hoverColor: '{indigo.300}',
-                    activeColor: '{ingido.200}'
-                },
-            },
-        }
+        primary: {
+            50: '{indigo.50}',
+            100: '{indigo.100}',
+            200: '{indigo.200}',
+            300: '{indigo.300}',
+            400: '{indigo.400}',
+            500: '{indigo.500}',
+            600: '{indigo.600}',
+            700: '{indigo.700}',
+            800: '{indigo.800}',
+            900: '{indigo.900}',
+            950: '{indigo.950}'
+        },
     }
 });
 
