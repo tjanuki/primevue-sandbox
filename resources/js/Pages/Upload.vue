0505671a74ad466d702fac56a981b5c953009498
tjanuki <t.januki@gmail.com>
2024-08-03 15:16:04 -0400

    Add Custom upload

--- resources/js/Pages/Upload.vue
+++ resources/js/Pages/Upload.vue
@@ -1,9 +1,10 @@
 <script setup>
 import DashboardLayout from "@/Layouts/DashboardLayout.vue";
 import {useForm} from "@inertiajs/vue3";
-import { ref } from 'vue';
+import {ref} from 'vue';
 import FileUpload from 'primevue/fileupload';
-import Button    from "primevue/button";
+import Button from "primevue/button";
+import {useToast} from "primevue/usetoast";
 
 const fileUploadRef = ref(null);
 const imageUrl = ref(null);
@@ -38,36 +39,28 @@ const onRemove = () => {
     }
 };
 
+const toast = useToast();
+
+const onAdvancedUpload = () => {
+    toast.add({severity: 'info', summary: 'Success', detail: 'File Uploaded', life: 3000});
+};
 </script>
 
 <template>
     <DashboardLayout title="Form" @submit.prevent="submit">
-        <form class="flex flex-col gap-10">
-            <h3>Image Uploader</h3>
+        <form class="flex flex-col gap-10 w-full items-center">
             <FileUpload
-                ref="fileUploadRef"
-                mode="advanced"
-                :customUpload="true"
-                @uploader="onUpload"
-                :auto="true"
-                chooseLabel="Select Image"
-                :maxFileSize="1000000"
+                name="demo[]"
+                url="/api/upload"
+                @upload="onAdvancedUpload($event)"
+                :multiple="true"
                 accept="image/*"
-                :multiple="false"
-                @clear="onClear"
-                @remove="onRemove"
+                :maxFileSize="1000000"
+                :auto="true"
+                :showUploadButton="false"
             >
-                <template #empty>
-                    <p v-if="!imageUrl">Drag and drop an image here to upload.</p>
-                </template>
-                <template #header="{ chooseCallback }">
-                    <Button @click="chooseCallback" class="p-button-primary" label="Select Image" icon="pi pi-plus" />
-                </template>
-                <template #content="{ files, removeFileCallback }">
-                    <div v-if="imageUrl" class="flex items-center mt-4">
-                        <img :src="imageUrl" alt="Uploaded image preview" class="w-24 h-24 object-fit mr-4" />
-                        <Button @click="() => removeFileCallback(0)" class="p-button-danger" icon="pi pi-times" />
-                    </div>
+                <template #empty class="bg-black">
+                    <span>Drag and drop files to here to upload.</span>
                 </template>
             </FileUpload>
         </form>
