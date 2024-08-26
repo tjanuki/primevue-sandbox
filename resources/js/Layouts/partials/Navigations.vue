8edf6ddee53c237ffd220619702ab26ced847920
tjanuki <t.januki@gmail.com>
2024-08-26 16:09:01 -0400

    Add MegaMenu

--- resources/js/Layouts/partials/Navigations.vue
+++ resources/js/Layouts/partials/Navigations.vue
@@ -1,56 +1,120 @@
-<script setup>
-import NavLink from '@/Components/NavLink.vue';
-</script>
-
 <template>
-    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-        <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-            Dashboard
-        </NavLink>
-        <NavLink :href="route('toast')" :active="route().current('toast')">
-            Toast
-        </NavLink>
-        <NavLink :href="route('modal')" :active="route().current('modal')">
-            Modal
-        </NavLink>
-        <NavLink :href="route('form')" :active="route().current('form')">
-            Form
-        </NavLink>
-        <NavLink :href="route('upload')" :active="route().current('upload')">
-            Upload
-        </NavLink>
-        <NavLink :href="route('crop')" :active="route().current('crop')">
-            Crop
-        </NavLink>
-        <NavLink :href="route('tabs')" :active="route().current('tabs')">
-            Tabs
-        </NavLink>
-        <NavLink :href="route('datatables')" :active="route().current('datatables')">
-            DataTable
-        </NavLink>
-        <NavLink :href="route('reorders')" :active="route().current('reorders')">
-            Reorders
-        </NavLink>
-        <NavLink :href="route('scroll-panels')" :active="route().current('scroll-panels')">
-            Scroll Panels
-        </NavLink>
-        <NavLink :href="route('menus')" :active="route().current('menus')">
-            Menus
-        </NavLink>
-        <NavLink :href="route('org-charts')" :active="route().current('org-charts')">
-            OrgChart
-        </NavLink>
-        <NavLink :href="route('diagrams')" :active="route().current('diagrams')">
-            Diagrams
-        </NavLink>
-        <NavLink :href="route('diagram-panels')" :active="route().current('diagram-panels')">
-            Diagram Panels
-        </NavLink>
-        <NavLink :href="route('timelines')" :active="route().current('timelines')">
-            Timelines
-        </NavLink>
-        <NavLink :href="route('carousel')" :active="route().current('carousel')">
-            Carousel
-        </NavLink>
-    </div>
+    <MegaMenu :model="items" :pt="{
+        root: {
+            style: 'border: none; margin-left: 1rem;'
+        },
+            itemLink: {
+                class: 'p-menuitem-link',
+                tabindex: 0,
+                'aria-hidden': null  // Remove aria-hidden
+            },
+        }" />
 </template>
+
+<script setup>
+import { ref } from "vue";
+import NavLink from '@/Components/NavLink.vue';
+import MegaMenu from 'primevue/megamenu';
+
+const items = ref([
+    {
+        label: 'Dashboard',
+        icon: 'pi pi-fw pi-home',
+        command: () => { window.location.href = route('dashboard'); }
+    },
+    {
+        label: 'Components',
+        icon: 'pi pi-fw pi-star',
+        items: [
+            [
+                {
+                    label: 'UI Components',
+                    items: [
+                        {
+                            label: 'Toast',
+                            command: () => { window.location.href = route('toast'); }
+                        },
+                        {
+                            label: 'Modal',
+                            command: () => { window.location.href = route('modal'); }
+                        },
+                        {
+                            label: 'Form',
+                            command: () => { window.location.href = route('form'); }
+                        },
+                        {
+                            label: 'Upload',
+                            command: () => { window.location.href = route('upload'); }
+                        },
+                        {
+                            label: 'Crop',
+                            command: () => { window.location.href = route('crop'); }
+                        }
+                    ]
+                }
+            ],
+            [
+                {
+                    label: 'Data Components',
+                    items: [
+                        {
+                            label: 'Tabs',
+                            command: () => { window.location.href = route('tabs'); }
+                        },
+                        {
+                            label: 'DataTable',
+                            command: () => { window.location.href = route('datatables'); }
+                        },
+                        {
+                            label: 'Reorders',
+                            command: () => { window.location.href = route('reorders'); }
+                        },
+                        {
+                            label: 'Scroll Panels',
+                            command: () => { window.location.href = route('scroll-panels'); }
+                        }
+                    ]
+                }
+            ],
+            [
+                {
+                    label: 'Advanced Components',
+                    items: [
+                        {
+                            label: 'Menus',
+                            command: () => { window.location.href = route('menus'); }
+                        },
+                        {
+                            label: 'OrgChart',
+                            command: () => { window.location.href = route('org-charts'); }
+                        },
+                        {
+                            label: 'Diagrams',
+                            command: () => { window.location.href = route('diagrams'); }
+                        },
+                        {
+                            label: 'Diagram Panels',
+                            command: () => { window.location.href = route('diagram-panels'); }
+                        }
+                    ]
+                }
+            ],
+            [
+                {
+                    label: 'Misc Components',
+                    items: [
+                        {
+                            label: 'Timelines',
+                            command: () => { window.location.href = route('timelines'); }
+                        },
+                        {
+                            label: 'Carousel',
+                            command: () => { window.location.href = route('carousel'); }
+                        }
+                    ]
+                }
+            ]
+        ]
+    }
+]);
+</script>
