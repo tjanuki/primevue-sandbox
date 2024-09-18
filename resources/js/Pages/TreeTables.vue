ba82cea17461621bf7e477b16b6cab6b673cb2a8
tjanuki <t.januki@gmail.com>
2024-09-18 14:12:23 -0400

    Add Toggle button to TreeTables

--- resources/js/Pages/TreeTables.vue
+++ resources/js/Pages/TreeTables.vue
@@ -1,67 +1,97 @@
 <script setup>
-import {ref, onMounted} from 'vue';
+import { ref, onMounted } from 'vue';
 import DashboardLayout from "@/Layouts/DashboardLayout.vue";
 import TreeTable from 'primevue/treetable';
 import Column from 'primevue/column';
 import InputText from "primevue/inputtext";
-import SelectButton from 'primevue/selectbutton';
+import Button from 'primevue/button';
 
 const nodes = ref([
     {
         key: '0',
-        data: {name: 'Fruits', size: '3 items', type: 'Category'},
+        data: { name: 'Fruits', size: '3 items', type: 'Category' },
         children: [
             {
                 key: '0-0',
-                data: {name: 'Apple', size: '5', type: 'Fruit'}
+                data: { name: 'Apple', size: '5', type: 'Fruit' }
             },
             {
                 key: '0-1',
-                data: {name: 'Banana', size: '10', type: 'Fruit'}
+                data: { name: 'Banana', size: '10', type: 'Fruit' }
             },
             {
                 key: '0-2',
-                data: {name: 'Orange', size: '5', type: 'Fruit'}
+                data: { name: 'Orange', size: '5', type: 'Fruit' }
             }
         ]
     },
     {
         key: '1',
-        data: {name: 'Vegetables', size: '2 items', type: 'Category'},
+        data: { name: 'Vegetables', size: '2 items', type: 'Category' },
         children: [
             {
                 key: '1-0',
-                data: {name: 'Carrot', size: '20', type: 'Vegetable'}
+                data: { name: 'Carrot', size: '20', type: 'Vegetable' }
             },
             {
                 key: '1-1',
-                data: {name: 'Tomato', size: '10', type: 'Vegetable'}
+                data: { name: 'Tomato', size: '10', type: 'Vegetable' }
             }
         ]
     }
 ]);
 
+const expandedKeys = ref({});
 const filters = ref({});
-const selectedKeys = ref({});
+
+const toggleAll = () => {
+    const allKeys = getAllKeys(nodes.value);
+    if (Object.keys(expandedKeys.value).length === allKeys.length) {
+        // If all are expanded, collapse all
+        expandedKeys.value = {};
+    } else {
+        // Otherwise, expand all
+        const newExpandedKeys = {};
+        allKeys.forEach(key => {
+            newExpandedKeys[key] = true;
+        });
+        expandedKeys.value = newExpandedKeys;
+    }
+};
+
+const getAllKeys = (nodes) => {
+    let keys = [];
+    nodes.forEach(node => {
+        keys.push(node.key);
+        if (node.children) {
+            keys = keys.concat(getAllKeys(node.children));
+        }
+    });
+    return keys;
+};
 </script>
 
 <template>
-    <DashboardLayout title="Form" @submit.prevent>
+    <DashboardLayout title="Tree Table" @submit.prevent>
         <div class="card">
             <TreeTable
-                v-model:selectionKeys="selectedKeys"
+                v-model:expandedKeys="expandedKeys"
                 :value="nodes"
                 :filters="filters"
-                selectionMode="checkbox"
+                filterMode="lenient"
                 tableStyle="min-width: 50rem"
             >
+                <template #header>
+                    <div class="flex justify-end">
+                        <Button @click="toggleAll" label="Show All" class="mb-4" />
+                    </div>
+                </template>
                 <Column field="name" header="Name" expander style="width: 34%">
                     <template #body="{ node }">
                         {{ node.data.name }}
                     </template>
-                    <template #filter="lenient">
-                        <InputText v-model="filters['name']" type="text" class="p-column-filter"
-                                   placeholder="Filter by name"/>
+                    <template #filter>
+                        <InputText v-model="filters['name']" type="text" class="p-column-filter" placeholder="Filter by name" />
                     </template>
                 </Column>
             </TreeTable>
