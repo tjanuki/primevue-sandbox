74ec7f1c4f49e7b4d3542bcc48ac46d49320df92
tjanuki <t.januki@gmail.com>
2024-09-18 14:14:11 -0400

    Add checkbox to TreeTables

--- resources/js/Pages/TreeTables.vue
+++ resources/js/Pages/TreeTables.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import {ref, onMounted} from 'vue';
 import DashboardLayout from "@/Layouts/DashboardLayout.vue";
 import TreeTable from 'primevue/treetable';
 import Column from 'primevue/column';
@@ -9,33 +9,33 @@ import Button from 'primevue/button';
 const nodes = ref([
     {
         key: '0',
-        data: { name: 'Fruits', size: '3 items', type: 'Category' },
+        data: {name: 'Fruits', size: '3 items', type: 'Category'},
         children: [
             {
                 key: '0-0',
-                data: { name: 'Apple', size: '5', type: 'Fruit' }
+                data: {name: 'Apple', size: '5', type: 'Fruit'}
             },
             {
                 key: '0-1',
-                data: { name: 'Banana', size: '10', type: 'Fruit' }
+                data: {name: 'Banana', size: '10', type: 'Fruit'}
             },
             {
                 key: '0-2',
-                data: { name: 'Orange', size: '5', type: 'Fruit' }
+                data: {name: 'Orange', size: '5', type: 'Fruit'}
             }
         ]
     },
     {
         key: '1',
-        data: { name: 'Vegetables', size: '2 items', type: 'Category' },
+        data: {name: 'Vegetables', size: '2 items', type: 'Category'},
         children: [
             {
                 key: '1-0',
-                data: { name: 'Carrot', size: '20', type: 'Vegetable' }
+                data: {name: 'Carrot', size: '20', type: 'Vegetable'}
             },
             {
                 key: '1-1',
-                data: { name: 'Tomato', size: '10', type: 'Vegetable' }
+                data: {name: 'Tomato', size: '10', type: 'Vegetable'}
             }
         ]
     }
@@ -43,6 +43,7 @@ const nodes = ref([
 
 const expandedKeys = ref({});
 const filters = ref({});
+const selectedKeys = ref({});
 
 const toggleAll = () => {
     const allKeys = getAllKeys(nodes.value);
@@ -76,22 +77,25 @@ const getAllKeys = (nodes) => {
         <div class="card">
             <TreeTable
                 v-model:expandedKeys="expandedKeys"
+                v-model:selectionKeys="selectedKeys"
                 :value="nodes"
                 :filters="filters"
                 filterMode="lenient"
+                selectionMode="checkbox"
                 tableStyle="min-width: 50rem"
             >
                 <template #header>
-                    <div class="flex justify-end">
-                        <Button @click="toggleAll" label="Show All" class="mb-4" />
+                    <div class="flex justify-end items-center">
+                        <Button @click="toggleAll" label="Show All"/>
                     </div>
                 </template>
-                <Column field="name" header="Name" expander style="width: 34%">
+                <Column field="name" header="Name" expander>
                     <template #body="{ node }">
                         {{ node.data.name }}
                     </template>
                     <template #filter>
-                        <InputText v-model="filters['name']" type="text" class="p-column-filter" placeholder="Filter by name" />
+                        <InputText v-model="filters['name']" type="text" class="p-column-filter"
+                                   placeholder="Filter by name"/>
                     </template>
                 </Column>
             </TreeTable>
